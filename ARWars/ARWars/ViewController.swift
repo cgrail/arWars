f99742793f06af4faf2f2a06dffa020681c7730e
Christian Grail <christian.grail@sap.com>
2017-12-08 18:33:57 +0100

    Add explosion particle system

--- ARWars/ARWars/ViewController.swift
+++ ARWars/ARWars/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -22,21 +22,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         loadTieFigher()
         
         sceneView.scene = SCNScene()
+        sceneView.scene.physicsWorld.contactDelegate = self
         sceneView.autoenablesDefaultLighting = true
         
         addNewTieFigher()
     }
     
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         shoot()
     }
     
-    
     private func shoot() {
+        
+        playSoundEffect(ofType: .laser)
+        
         let node = SCNNode()
         
-        let box = SCNBox(width: 0.1, height: 0.1, length: 5, chamferRadius: 0)
+        let box = SCNBox(width: 0.1, height: 0.1, length: 1, chamferRadius: 0)
         box.firstMaterial?.diffuse.contents = UIColor.red
         box.firstMaterial?.lightingModel = .constant
         
@@ -46,10 +48,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let shape = SCNPhysicsShape(geometry: box, options: nil)
         node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: shape)
         node.physicsBody?.isAffectedByGravity = false
+        node.physicsBody?.categoryBitMask = CollisionCategory.laser.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionCategory.fighter.rawValue
         
         if let pov = sceneView.pointOfView {
             node.position = pov.position
-            node.position.y -= 0.5
+            
             node.eulerAngles = pov.eulerAngles
         }
         
@@ -74,15 +78,76 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         if let fighter = tieFighter?.clone() {
             let posX = floatBetween(-1, and: 1)
             let posY = floatBetween(-1, and: 1)
-            fighter.position = SCNVector3(posX, posY, -1) // SceneKit/AR coordinates are in meters
+            var targetPos = SCNVector3(posX, posY, -10)
+            fighter.position = targetPos
+
+            fighter.physicsBody = SCNPhysicsBody.dynamic()
+            fighter.physicsBody?.isAffectedByGravity = false
+
+            fighter.physicsBody?.categoryBitMask = CollisionCategory.fighter.rawValue
+            fighter.physicsBody?.contactTestBitMask = CollisionCategory.laser.rawValue
+            
+            targetPos.z = -1
+            let action = SCNAction.move(to: targetPos, duration: 1)
+            action.timingMode = .easeInEaseOut
+            fighter.runAction(action)
+            fighter.opacity = 0
+            fighter.runAction(SCNAction.fadeOpacity(to: 1, duration: 1))
+
             sceneView.scene.rootNode.addChildNode(fighter)
             self.fighterClone = fighter
+        }
+    }
+    
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        if contact.nodeA.physicsBody?.categoryBitMask == CollisionCategory.fighter.rawValue || contact.nodeB.physicsBody?.categoryBitMask == CollisionCategory.fighter.rawValue {
+            
+            self.playSoundEffect(ofType: .explosion)
+            
+            let particleSystem = SCNParticleSystem(named: "Explode", inDirectory: nil)
+            let systemNode = SCNNode()
+            systemNode.addParticleSystem(particleSystem!)
+            systemNode.position = contact.nodeA.position
+            sceneView.scene.rootNode.addChildNode(systemNode)
+            
+            contact.nodeA.removeFromParentNode()
+            contact.nodeB.removeFromParentNode()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                self.addNewTieFigher()
+            })
         }
     }
     
     func loadTieFigher() {
         if let scene = SCNScene(named: "art.scnassets/Tie.scn") {
             tieFighter = scene.rootNode.childNodes[0]
+        }
+    }
+    
+    private var player: AVAudioPlayer?
+    
+    enum SoundEffect: String {
+        case laser = "Laser"
+        case explosion = "Explosion"
+    }
+    
+    func playSoundEffect(ofType effect: SoundEffect) {
+        
+        // Async to avoid substantial cost to graphics processing (may result in sound effect delay however)
+        DispatchQueue.main.async {
+            do
+            {
+                if let effectURL = Bundle.main.url(forResource: effect.rawValue, withExtension: "mp3") {
+                    
+                    self.player = try AVAudioPlayer(contentsOf: effectURL)
+                    self.player?.play()
+                    
+                }
+            }
+            catch let error as NSError {
+                print(error.description)
+            }
         }
     }
     
@@ -107,4 +172,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+}
+
+struct CollisionCategory: OptionSet {
+    let rawValue: Int
+    
+    static let laser  = CollisionCategory(rawValue: 1 << 0) // 00...01
+    static let fighter = CollisionCategory(rawValue: 1 << 1) // 00..10
 }
